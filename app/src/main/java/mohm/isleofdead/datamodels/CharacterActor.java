6fc50362a69b0565a7b09c6c45071414b86a87e1
Fayz1234 <30286264+Fayz1234@users.noreply.github.com>
2018-04-19 16:22:28 +1000

    Add files via upload

---
+++ app/src/main/java/mohm/isleofdead/datamodels/CharacterActor.java
@@ -0,0 +1,21 @@
+package isleofdead.mohm.isleofdead.datamodels;
+
+
+import android.media.Image;
+
+import java.io.Serializable;
+
+public class CharacterActor implements Serializable
+{
+    public String actor_name;
+    public String type;
+    public int base_health;
+    public int current_health;
+    public int strength;
+    public int dexterity;
+    public int constitution;
+    public int defense;
+    public int willpower;
+    public Image graphic;
+
+}
