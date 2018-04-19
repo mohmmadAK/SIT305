9fa01e4f26d466ec887122f9d187f6debd9f9e0c
Fayz1234 <30286264+Fayz1234@users.noreply.github.com>
2018-04-19 16:46:15 +1000

    Add files via upload
    Data Model for a game quest

---
+++ app/src/main/java/mohm/isleofdead/datamodels/Quest.java
@@ -0,0 +1,31 @@
+package isleofdead.mohm.isleofdead.datamodels;
+
+
+import java.io.Serializable;
+
+public class Quest implements Serializable
+{
+    int quest_id;
+    String quest_name;
+    String quest_description;
+    String item_name;
+    int count;
+    Boolean isActive;
+    Boolean isComplete;
+    int get_reward;
+    Item item_reward;
+
+
+    public Quest (int id,String qname, String description,String iname, int er, Item ir)
+    {
+        quest_id = id;
+        quest_name = qname;
+        quest_description = description;
+        count = 0;
+        isActive = true;
+        isComplete = false;
+        item_name = iname;
+        get_reward = er;
+        item_reward = ir;
+    }
+}
