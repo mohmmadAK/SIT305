e152fb50224592ea031fb1ce6d81be977125df4a
Fayz1234 <30286264+Fayz1234@users.noreply.github.com>
2018-04-19 16:47:39 +1000

    Add files via upload
    Data model for Game Weapon

---
+++ app/src/main/java/mohm/isleofdead/datamodels/Weapon.java
@@ -0,0 +1,23 @@
+package isleofdead.mohm.isleofdead.datamodels;
+
+import android.media.Image;
+
+import java.io.Serializable;
+
+public class Weapon extends Item implements Serializable
+{
+
+    public Weapon(String n, Image i, String d, int[] ss, int gold_value)
+    {
+        super(n);
+        icon = i;
+        description = d;
+        stats = ss;
+        value = gold_value;
+    }
+
+    public int[] getStatBuffs()
+    {
+        return stats;
+    }
+}
