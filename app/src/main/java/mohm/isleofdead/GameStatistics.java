a94c8aea73c3812a748e20e34538d098c3055d74
mohmmadAK <M.ALKHUZAMI@GMAIL.COM>
2018-05-03 12:23:16 +1000

    Add files via upload
    Game Statistics Activity

---
+++ app/src/main/java/mohm/isleofdead/GameStatistics.java
@@ -0,0 +1,29 @@
+package isleofdead.mohm.isleofdead;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class GameStatistics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+
+        TextView name =(TextView) findViewById(R.id.tvCharacterName);
+        TextView attack =(TextView) findViewById(R.id.tvAttackPower);
+        TextView health =(TextView) findViewById(R.id.tvHealth);
+        TextView hunger =(TextView) findViewById(R.id.tvhunger);
+        TextView killed =(TextView) findViewById(R.id.tvEnemysKilled);
+
+        Intent info = getIntent();
+
+        name.setText("Name: " + info.getStringExtra("name"));
+        attack.setText("Attack Power: " + String.valueOf(info.getIntExtra("attack", 0)));
+        health.setText("Health: " + String.valueOf(info.getIntExtra("hp", 0)));
+        hunger.setText("Hunger: " + String.valueOf(info.getIntExtra("hunger", 0)));
+        killed.setText("Enemies Killed: " + String.valueOf(info.getIntExtra("killed", 0)));
+    }
+}
